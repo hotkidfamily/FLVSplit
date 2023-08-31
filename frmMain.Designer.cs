5d573e78620bfe94c8a9ff40af3cbf074ee15460
yanli <yanli@yy.com>
2023-08-31 11:35:02 +0800

    feat: change icon

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -1,4 +1,4 @@
-namespace JDP {
+﻿namespace JDP {
 	partial class frmMain {
 		/// <summary>
 		/// Required designer variable.
@@ -23,113 +23,116 @@ namespace JDP {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-            this.btnAbout = new System.Windows.Forms.Button();
-            this.lblInstructions = new System.Windows.Forms.Label();
-            this.grpExtract = new System.Windows.Forms.GroupBox();
-            this.chkTranscode = new System.Windows.Forms.CheckBox();
-            this.chkAudio = new System.Windows.Forms.CheckBox();
-            this.chkTimeCodes = new System.Windows.Forms.CheckBox();
-            this.chkVideo = new System.Windows.Forms.CheckBox();
-            this.grpExtract.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // btnAbout
-            // 
-            this.btnAbout.Location = new System.Drawing.Point(161, 143);
-            this.btnAbout.Name = "btnAbout";
-            this.btnAbout.Size = new System.Drawing.Size(88, 44);
-            this.btnAbout.TabIndex = 2;
-            this.btnAbout.Text = "A&bout";
-            this.btnAbout.UseVisualStyleBackColor = true;
-            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
-            // 
-            // lblInstructions
-            // 
-            this.lblInstructions.Location = new System.Drawing.Point(8, 8);
-            this.lblInstructions.Name = "lblInstructions";
-            this.lblInstructions.Size = new System.Drawing.Size(408, 56);
-            this.lblInstructions.TabIndex = 0;
-            this.lblInstructions.Text = "Drop FLV files here.\r\n\r\nOutput files are written in the same folder as the FLVs.";
-            this.lblInstructions.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // grpExtract
-            // 
-            this.grpExtract.Controls.Add(this.chkTranscode);
-            this.grpExtract.Controls.Add(this.chkAudio);
-            this.grpExtract.Controls.Add(this.chkTimeCodes);
-            this.grpExtract.Controls.Add(this.chkVideo);
-            this.grpExtract.Location = new System.Drawing.Point(8, 81);
-            this.grpExtract.Name = "grpExtract";
-            this.grpExtract.Size = new System.Drawing.Size(147, 176);
-            this.grpExtract.TabIndex = 1;
-            this.grpExtract.TabStop = false;
-            this.grpExtract.Text = "Extract:";
-            // 
-            // chkTranscode
-            // 
-            this.chkTranscode.Checked = true;
-            this.chkTranscode.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkTranscode.Location = new System.Drawing.Point(12, 112);
-            this.chkTranscode.Name = "chkTranscode";
-            this.chkTranscode.Size = new System.Drawing.Size(108, 21);
-            this.chkTranscode.TabIndex = 3;
-            this.chkTranscode.Text = "Transcode";
-            this.chkTranscode.UseVisualStyleBackColor = true;
-            // 
-            // chkAudio
-            // 
-            this.chkAudio.Checked = true;
-            this.chkAudio.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkAudio.Location = new System.Drawing.Point(12, 85);
-            this.chkAudio.Name = "chkAudio";
-            this.chkAudio.Size = new System.Drawing.Size(108, 21);
-            this.chkAudio.TabIndex = 2;
-            this.chkAudio.Text = "Audio";
-            this.chkAudio.UseVisualStyleBackColor = true;
-            // 
-            // chkTimeCodes
-            // 
-            this.chkTimeCodes.Checked = true;
-            this.chkTimeCodes.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkTimeCodes.Location = new System.Drawing.Point(12, 55);
-            this.chkTimeCodes.Name = "chkTimeCodes";
-            this.chkTimeCodes.Size = new System.Drawing.Size(108, 21);
-            this.chkTimeCodes.TabIndex = 1;
-            this.chkTimeCodes.Text = "Timecodes";
-            this.chkTimeCodes.UseVisualStyleBackColor = true;
-            // 
-            // chkVideo
-            // 
-            this.chkVideo.Checked = true;
-            this.chkVideo.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkVideo.Location = new System.Drawing.Point(12, 25);
-            this.chkVideo.Name = "chkVideo";
-            this.chkVideo.Size = new System.Drawing.Size(108, 21);
-            this.chkVideo.TabIndex = 0;
-            this.chkVideo.Text = "Video";
-            this.chkVideo.UseVisualStyleBackColor = true;
-            // 
-            // frmMain
-            // 
-            this.AllowDrop = true;
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(551, 269);
-            this.Controls.Add(this.grpExtract);
-            this.Controls.Add(this.lblInstructions);
-            this.Controls.Add(this.btnAbout);
-            this.Font = new System.Drawing.Font("΢���ź�", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.Name = "frmMain";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FLV Extract";
-            this.TopMost = true;
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
-            this.Load += new System.EventHandler(this.frmMain_Load);
-            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
-            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
-            this.grpExtract.ResumeLayout(false);
-            this.ResumeLayout(false);
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+			this.btnAbout = new System.Windows.Forms.Button();
+			this.lblInstructions = new System.Windows.Forms.Label();
+			this.grpExtract = new System.Windows.Forms.GroupBox();
+			this.chkTranscode = new System.Windows.Forms.CheckBox();
+			this.chkAudio = new System.Windows.Forms.CheckBox();
+			this.chkTimeCodes = new System.Windows.Forms.CheckBox();
+			this.chkVideo = new System.Windows.Forms.CheckBox();
+			this.grpExtract.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btnAbout
+			// 
+			this.btnAbout.Location = new System.Drawing.Point(440, 123);
+			this.btnAbout.Name = "btnAbout";
+			this.btnAbout.Size = new System.Drawing.Size(88, 44);
+			this.btnAbout.TabIndex = 2;
+			this.btnAbout.Text = "A&bout";
+			this.btnAbout.UseVisualStyleBackColor = true;
+			this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+			// 
+			// lblInstructions
+			// 
+			this.lblInstructions.Location = new System.Drawing.Point(8, 8);
+			this.lblInstructions.Name = "lblInstructions";
+			this.lblInstructions.Size = new System.Drawing.Size(471, 70);
+			this.lblInstructions.TabIndex = 0;
+			this.lblInstructions.Text = "✔️\tDrop FLV files here.\r\n✔️把文件丢在这里.\r\n\r\n❗Output files are written in the same fold" +
+    "er as the FLVs.\r\n";
+			this.lblInstructions.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// grpExtract
+			// 
+			this.grpExtract.Controls.Add(this.chkTranscode);
+			this.grpExtract.Controls.Add(this.chkAudio);
+			this.grpExtract.Controls.Add(this.chkTimeCodes);
+			this.grpExtract.Controls.Add(this.chkVideo);
+			this.grpExtract.Location = new System.Drawing.Point(8, 81);
+			this.grpExtract.Name = "grpExtract";
+			this.grpExtract.Size = new System.Drawing.Size(147, 147);
+			this.grpExtract.TabIndex = 1;
+			this.grpExtract.TabStop = false;
+			this.grpExtract.Text = "Extract:";
+			// 
+			// chkTranscode
+			// 
+			this.chkTranscode.Checked = true;
+			this.chkTranscode.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.chkTranscode.Location = new System.Drawing.Point(12, 112);
+			this.chkTranscode.Name = "chkTranscode";
+			this.chkTranscode.Size = new System.Drawing.Size(108, 21);
+			this.chkTranscode.TabIndex = 3;
+			this.chkTranscode.Text = "ToMP4";
+			this.chkTranscode.UseVisualStyleBackColor = true;
+			// 
+			// chkAudio
+			// 
+			this.chkAudio.Checked = true;
+			this.chkAudio.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.chkAudio.Location = new System.Drawing.Point(12, 85);
+			this.chkAudio.Name = "chkAudio";
+			this.chkAudio.Size = new System.Drawing.Size(108, 21);
+			this.chkAudio.TabIndex = 2;
+			this.chkAudio.Text = "Audio";
+			this.chkAudio.UseVisualStyleBackColor = true;
+			// 
+			// chkTimeCodes
+			// 
+			this.chkTimeCodes.Checked = true;
+			this.chkTimeCodes.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.chkTimeCodes.Location = new System.Drawing.Point(12, 55);
+			this.chkTimeCodes.Name = "chkTimeCodes";
+			this.chkTimeCodes.Size = new System.Drawing.Size(108, 21);
+			this.chkTimeCodes.TabIndex = 1;
+			this.chkTimeCodes.Text = "Timecodes";
+			this.chkTimeCodes.UseVisualStyleBackColor = true;
+			// 
+			// chkVideo
+			// 
+			this.chkVideo.Checked = true;
+			this.chkVideo.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.chkVideo.Location = new System.Drawing.Point(12, 25);
+			this.chkVideo.Name = "chkVideo";
+			this.chkVideo.Size = new System.Drawing.Size(108, 21);
+			this.chkVideo.TabIndex = 0;
+			this.chkVideo.Text = "Video";
+			this.chkVideo.UseVisualStyleBackColor = true;
+			// 
+			// frmMain
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(551, 233);
+			this.Controls.Add(this.grpExtract);
+			this.Controls.Add(this.lblInstructions);
+			this.Controls.Add(this.btnAbout);
+			this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "frmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "FLV Extract";
+			this.TopMost = true;
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+			this.grpExtract.ResumeLayout(false);
+			this.ResumeLayout(false);
 
 		}
 
